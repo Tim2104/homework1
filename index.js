43cfda7b511c8888f5a8438a657b4a6fccf214c6
Tim2104 <rostov-r89@mail.ru>
2022-07-03 19:10:23 +0300

    ...

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const args = yargs
 .option('dist', {
     alias : 'd',
     describe: 'путь ко второму каталогу',
-    default: './dist'
+    default: '/new'
 })
 .option('delete', {
     alias : 'D',
@@ -34,7 +34,6 @@ const config = {
     isDelete: args.delete
 };
 
-let folders = [];
 
 function readDir(src) {
     fs.readdir(src, (err, array) => {
@@ -56,16 +55,21 @@ function readDir(src) {
                     let newDir = config.dist + '/' + path.parse(curPath).name.toUpperCase();
 
                     if(!fs.existsSync(newDir)) {
+
+                        if(!config.dist) {
+                            fs.mkdir(config.dist, {recursive: false}, (err) => {
+                                if(err) throw err;
+                            });
+                        }
+
                         fs.mkdir(newDir, {recursive: false}, (err) => {
-                            if(err) throw err;
+                            if(err) console.log(`directory ${config.dist} not found!`);
                         });
 
                         let newfile = newDir + '/' + path.parse(curPath).base;
 
                         fs.link(curPath, newfile, (err) => {
                             if(err) throw err;
-
-                            console.log('copy' + curPath);
                         });
 
                     }
@@ -99,50 +103,21 @@ function removeFile(src) {
                     fs.unlink(curPath, (err) => {
                         if(err) throw err;
                     });
-
-                    console.log('delete file' + curPath);
                 }
             });
         }
     });
 }
 
-function removeDir(src) {
-
-    fs.readdir(src, (err, array) => {
+if(!fs.existsSync(config.dist)) {
+    fs.mkdir(config.dist, {recursive: false}, (err) => {
         if(err) throw err;
-        
-        for(let n = 0; n < array.length; n++) {
-            let elem = array[n];
-
-            let curPath = path.resolve(src, elem);
-
-            fs.stat(curPath, (err, elem) => {
-                if(err) throw err;
-
-                if(elem.isDirectory()) {
-
-                    folders.unshift(curPath);
-                    removeDir(curPath);
-                }
-            });
-
-        }
     });
-    for(let i = 0; i < folders.length; i++) {
-
-        fs.rmdir(folders[i], (err) => {
-            if(err) return;
-        });
-
-        console.log('delete dir');
-    }
 }
 
 readDir(config.entry);
 
 if(config.isDelete) {
-    removeDir(config.entry);
     removeFile(config.entry);
     
 }
